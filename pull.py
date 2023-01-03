af102772ba88de40e02f88d822549d3562583742
Robin Harvey <harvey.robin@gmail.com>
2023-01-03 22:00:14 +0000

    disco

--- pull.py
+++ pull.py
@@ -85,7 +85,18 @@ class Xml2Json:
 
 
 
-def xml_to_native(from_file: str, force_list_element: str=""):
+def xml_to_native(from_file: str, force_list_elements: list[str]=None, aliases: dict=None, protos: dict=None):
+    """Attempt a generic mapping of the given XML document to a native Python dict/list structure.
+
+    Not all valid XML will work here, complex content is not supported.  Name clashes between attributes and child
+    elements wil result in data loss - child element wins (TODO: Fix this).
+
+    Args:
+        from_file (str): XML data is in this file
+        force_list_element (list[str], optional): Force a list type for these element names. Defaults to None.
+        aliases (dict, optional): Optional aliases to change mapped dict keys. Defaults to None.
+        protos (dict, optional): Optional prototype objects for the named elements. Defaults to None.
+    """
     def _push_lineage(key, item):
         nonlocal lineage, curr_node, curr_key
         lineage.append((key, item))
@@ -100,6 +111,9 @@ def xml_to_native(from_file: str, force_list_element: str=""):
             # log.debug("bodge?")
             curr_key, curr_node = None, root
 
+    aliases = {} if aliases is None else aliases
+    protos = {} if protos is None else protos
+    force_list_elements = [] if force_list_elements is None else force_list_elements
     xmldoc = pulldom.parse(from_file)
     root = curr_node = {}
     curr_key = None
@@ -108,16 +122,18 @@ def xml_to_native(from_file: str, force_list_element: str=""):
         _event = xmldoc.getEvent()
         match _event:
             case [pulldom.START_ELEMENT, xml_node]:
-                myself = {} if len(xml_node.attributes) == 0 else {x: y for x, y in xml_node.attributes.items()}
-                if xml_node.nodeName in curr_node:
-                    if isinstance(curr_node[xml_node.nodeName], list) is False:
-                        curr_node[xml_node.nodeName] = [curr_node[xml_node.nodeName]]
-                    curr_node[xml_node.nodeName].append(myself)
-                elif xml_node.nodeName == force_list_element:
-                    curr_node[xml_node.nodeName] = [myself]
+                myself = protos[xml_node.nodeName] if xml_node.nodeName in protos else {}
+                myself = myself | {x: y for x, y in xml_node.attributes.items()}
+                node_name = aliases[xml_node.nodeName] if xml_node.nodeName in aliases else xml_node.nodeName
+                if node_name in curr_node:
+                    if isinstance(curr_node[node_name], list) is False:
+                        curr_node[node_name] = [curr_node[node_name]]
+                    curr_node[node_name].append(myself)
+                elif node_name in force_list_elements:
+                    curr_node[node_name] = [myself]
                 else:
-                    curr_node[xml_node.nodeName] = myself
-                _push_lineage(xml_node.nodeName, myself)
+                    curr_node[node_name] = myself
+                _push_lineage(node_name, myself)
                 log.debug(pulldom.START_ELEMENT, xml_node=xml_node)
             case [pulldom.END_ELEMENT, xml_node]:
                 _pop_lineage()
@@ -159,7 +175,21 @@ def xml_to_native(from_file: str, force_list_element: str=""):
 
 if __name__ == "__main__":
     #native = xj.to_native2("sample/page-6.xml")
-    native = xml_to_native("sample/page-6.xml", force_list_element="String")
+    # This prototype is used for all SP elements to make the resulting objects have the same keys for both
+    # String and SP elements.  Combined with the alias, this means that we get a single collection of String
+    # / SP elements in the correct order rather than 2 sibling sub-collections of each TextLine
+    prototype_sp = {
+        "ID": None,
+        "CONTENT": None,
+        "WC": None,
+        "HEIGHT": None,
+    }
+    native = xml_to_native(
+        "sample/page-6.xml",
+        force_list_element=["String"],
+        aliases={"SP": "String"},
+        protos={"SP": prototype_sp},
+    )
     json_data = json.dumps(native)
     log.info("eventual native", _is=native, json=json_data)
     print(json_data)
